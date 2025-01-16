c30e82a70b817cf19d61b3722bffd474200bab27
thanhlonq23 <nguyenthanhlonq23@gmail.com>
2025-01-16 23:47:57 +0700

    a

--- Spring Boot/helloworld/src/main/java/com/nguyenlonq23/spring/helloworld/HelloWorldController.java
+++ Spring Boot/helloworld/src/main/java/com/nguyenlonq23/spring/helloworld/HelloWorldController.java
@@ -1,10 +1,17 @@
 package com.nguyenlonq23.spring.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    @Value("${khoahoc.name}")
+    private String name;
+    @Value("${khoahoc.web}")
+    private String web;
+
+
     @GetMapping("/")
     public String index() {
         return "Hello World";
@@ -12,6 +19,6 @@ public class HelloWorldController {
 
     @GetMapping("/index2")
     public String index2() {
-        return "Xin chào";
+        return "name: " + name + ", web: " + web;
     }
 }
