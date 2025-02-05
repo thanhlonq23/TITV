e46f9ac82d62dc9b0cf2b2f389c9a5c37b9b2201
thanhlonq23 <nguyenthanhlonq23@gmail.com>
2025-02-06 01:20:31 +0700

    lesson 6.3

---
+++ Spring Boot/HibernateAndJPA/src/test/java/com/nguyenthanhlong/hibernateandjpa/HibernateAndJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenthanhlong.hibernateandjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateAndJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
