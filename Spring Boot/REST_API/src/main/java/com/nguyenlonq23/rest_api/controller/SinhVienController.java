16d3cd779a465a1bd559836b076194b3a5985340
thanhlonq23 <nguyenthanhlonq23@gmail.com>
2025-02-09 20:45:27 +0700

    lesson 7.8

--- Spring Boot/REST_API/src/main/java/com/nguyenlonq23/rest_api/controller/SinhVienController.java
+++ Spring Boot/REST_API/src/main/java/com/nguyenlonq23/rest_api/controller/SinhVienController.java
@@ -3,11 +3,15 @@ package com.nguyenlonq23.rest_api.controller;
 import com.nguyenlonq23.rest_api.entity.SinhVien;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/sinhvien")
@@ -20,12 +24,19 @@ public class SinhVienController {
         students.add(new SinhVien(1, "Long", 21, "CNTT", 8));
         students.add(new SinhVien(2, "Long2", 22, "KE", 7));
         students.add(new SinhVien(3, "Long3", 23, "QLD", 6));
-        students.add(new SinhVien(4, "Long4", 24, "NNA", 5));
-        students.add(new SinhVien(5, "Long5", 25, "MT", 8));
+        students.add(new SinhVien(4, "Long4", 23, "CNTT", 5));
+        students.add(new SinhVien(5, "Long5", 25, "CNTT", 8));
     }
 
     @GetMapping("/all")
     public List<SinhVien> getAllStudents() {
         return students;
+    }
+
+    @GetMapping("/get/{id}")
+    public List<SinhVien> getStudent(@PathVariable int id) {
+        List<SinhVien> result = students.stream().filter(sv -> sv.getNganhHoc() == "CNTT" && sv.getTuoi() <= 23).collect(Collectors.toList());
+        result.sort(Comparator.comparing(SinhVien::getId));
+        return result;
     }
 }
--- Spring Boot/REST_API/src/main/java/com/nguyenlonq23/rest_api/demoController.java
+++ Spring Boot/REST_API/src/main/java/com/nguyenlonq23/rest_api/demoController.java
@@ -1,6 +1,7 @@
 package com.nguyenlonq23.rest_api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class demoController {
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "Hello index!!!";
     }
+
     @GetMapping("/1")
-    public String hello(){
+    public String hello() {
         return "Hello world!!";
+    }
+
+    @GetMapping("/2/{id}")
+    public String getId(@PathVariable int id) {
+        return "Id is: " + id;
     }
 }
